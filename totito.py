4fb3b4a082312ee27ad0adc50717f7fdc59f339b
joshuaavalos74 <joshuaavalos74@gmail.com>
2016-07-13 08:49:12 -0600

    yaysya

--- totito.py
+++ totito.py
@@ -1,1 +1,16 @@
 print("Bienvenido al super totito de Naruto.")
+lista= [["a", "b", "c"],
+		["d", "e", "f"],
+		["g", "h", "i"]
+		]
+print (lista[0])
+print (lista[1])
+print (lista[2])
+sig=0
+while==sig:
+	1=input("J1=1 ")
+	t1v=ord(t1)-65
+	t1f=t1v%3
+	t1c=t1v//3
+	lista[t1f[t1c]]=0
+
